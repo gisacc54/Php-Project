b8e9877fc27646edfe07e0cb32f7d72f2428f2da
gisacc54 <giftisacc54@gmail.com>
2019-10-15 01:47:34 +0300

    Dashboard 2 and login session

---
+++ dashboard2.php
@@ -0,0 +1,140 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Dashbord</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" type="text/css" href="asset/css/dashboard2.css">
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle navbar-toggle-sidebar collapsed">
+      MENU
+      </button>
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        Administrator
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
+      <form class="navbar-form navbar-left" method="GET" role="search">
+        <div class="form-group">
+          <input type="text" name="q" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" target="_blank">Visit Site</a></li>
+        <li class="dropdown ">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            Account
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li class="dropdown-header"><?=$_SESSION['email']?></li>
+              <li class=""><a href="#">Profile</a></li>
+              <li class=""><a href="#">Notification</a></li>
+              <li class=""><a href="#">Setting</a></li>
+              <li class="divider"></li>
+              <li><a href="login_form.php">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>    <div class="container-fluid main-container">
+      <div class="col-md-2 sidebar">
+        <div class="row">
+  <!-- uncomment code for absolute positioning tweek see top comment in css -->
+  <div class="absolute-wrapper"> </div>
+  <!-- Menu -->
+  <div class="side-menu">
+    <nav class="navbar navbar-default" role="navigation">
+      <!-- Main Menu -->
+      <div class="side-menu-container">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-plane"></span> Active Link</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-cloud"></span> Link</a></li>
+
+          <!-- Dropdown-->
+          <li class="panel panel-default" id="dropdown">
+            <a data-toggle="collapse" href="#dropdown-lvl1">
+              <span class="glyphicon glyphicon-user"></span> Sub Level <span class="caret"></span>
+            </a>
+
+            <!-- Dropdown level 1 -->
+            <div id="dropdown-lvl1" class="panel-collapse collapse">
+              <div class="panel-body">
+                <ul class="nav navbar-nav">
+                  <li><a href="#">Link</a></li>
+                  <li><a href="#">Link</a></li>
+                  <li><a href="#">Link</a></li>
+
+                  <!-- Dropdown level 2 -->
+                  <li class="panel panel-default" id="dropdown">
+                    <a data-toggle="collapse" href="#dropdown-lvl2">
+                      <span class="glyphicon glyphicon-off"></span> Sub Level <span class="caret"></span>
+                    </a>
+                    <div id="dropdown-lvl2" class="panel-collapse collapse">
+                      <div class="panel-body">
+                        <ul class="nav navbar-nav">
+                          <li><a href="#">Link</a></li>
+                          <li><a href="#">Link</a></li>
+                          <li><a href="#">Link</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </li>
+
+          <li><a href="#"><span class="glyphicon glyphicon-signal"></span> Link</a></li>
+
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </nav>
+
+  </div>
+</div>      </div>
+      <div class="col-md-10 content">
+          <div class="panel panel-default">
+  <div class="panel-heading">
+    Dashboard
+  </div>
+  <div class="panel-body">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </div>
+</div>
+      </div>
+      <footer class="pull-left footer">
+        <p class="col-md-12" >
+          <hr class="divider" >
+          Copyright &COPY; 2018 <a  href="http://www.gossystem.com">GOS Systems</a>
+        </p>
+      </footer>
+    </div>
+</body>
+</html>
